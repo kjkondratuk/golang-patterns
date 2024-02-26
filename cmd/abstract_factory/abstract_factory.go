4aa4ec1e203e7c41a06bf3bb3a1fdc24de17ea29
Kevin Kondratuk <k.kondratuk@fetchrewards.com>
2024-02-26 10:18:31 -0600

    functional args and abstract factory

--- cmd/abstract_factory/abstract_factory.go
+++ cmd/abstract_factory/abstract_factory.go
@@ -1,5 +1,70 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"os"
+)
 
+type genericService struct {
+	msg string
+}
+
+type GenericService interface {
+	GetValue() string
+}
+
+func (s *genericService) GetValue() string {
+	return s.msg
+}
+
+// var _ GenericService = localService{}
+type localService struct {
+	genericService
+}
+
+func (s *localService) GetValue() string {
+	return s.msg
+}
+
+// var _ GenericService = deployedService{}
+type deployedService struct {
+	genericService
+}
+
+func (s *deployedService) GetValue() string {
+	return s.msg
+}
+
+// TODO: implement an extensible New function that allows the caller to define a new GenericService implementation
+func New(env string) GenericService {
+	if env == "local" {
+		return &localService{genericService{"local implementation"}}
+	} else if env == "deployed" {
+		return &deployedService{genericService{"deployed implementation"}}
+	} else {
+		return &genericService{"default implementation"}
+	}
+}
+
+func main() {
+	//services := []GenericService{
+	//	&genericService{"default implementation"},
+	//	&localService{genericService{"local implementation"}},
+	//	&deployedService{genericService{"deployed implementation"}},
+	//}
+
+	//services := []GenericService{
+	//	New("default"),
+	//	New("local"),
+	//	New("deployed"),
+	//}
+	//
+	//for _, s := range services {
+	//	fmt.Printf("%s\n", s.GetValue())
+	//}
+
+	v := os.Getenv("ENV")
+
+	s := New(v)
+	fmt.Printf("%s\n", s.GetValue())
 }
--- cmd/functional_args/functional_args.go
+++ cmd/functional_args/functional_args.go
@@ -1,5 +1,79 @@
 package main
 
+import "fmt"
+
+type myStruct struct {
+	// required
+	structValue string
+
+	// optional array
+	supplementalValues []string
+
+	// optional complex value
+	cval complexValue
+}
+
+type complexValue struct {
+	value1 string
+	value2 string
+}
+
+type mutableOption func(s *myStruct)
+type complexValueOption func(s complexValue) complexValue
+
+type unmutableOption func(s myStruct) myStruct
+
+func WithSupplementalValues(vals ...string) unmutableOption {
+	return func(s myStruct) myStruct {
+		s.supplementalValues = vals
+		return s
+	}
+}
+
+func WithComplexValue(cv ...complexValueOption) unmutableOption {
+	return func(s myStruct) myStruct {
+		for _, o := range cv {
+			s.cval = o(s.cval)
+		}
+		return s
+	}
+}
+
+func BadNew(sval string, cval complexValue, supplementalValues []string) *myStruct {
+	return &myStruct{
+		structValue:        sval,
+		supplementalValues: supplementalValues,
+		cval:               cval,
+	}
+}
+
+func GoodNew(sval string, opts ...unmutableOption) *myStruct {
+	s := myStruct{
+		structValue: sval,
+		cval: complexValue{
+			value2: "default value",
+		},
+	}
+	for _, opt := range opts {
+		s = opt(s)
+	}
+	return &s
+}
+
 func main() {
+	v1 := BadNew("my string value", complexValue{}, nil)
+	fmt.Printf("%+v\n", v1)
+
+	v2 := GoodNew("my string value")
+	fmt.Printf("%+v\n", v2)
+
+	v3 := GoodNew("my other string value", WithComplexValue(func(s complexValue) complexValue {
+		s.value1 = "some value"
+		return s
+	}))
+	fmt.Printf("%+v\n", v3)
+
+	v4 := GoodNew("my other string value", WithSupplementalValues("a supplemental value", "a supplemental value 2"))
+	fmt.Printf("%+v\n", v4)
 
 }
